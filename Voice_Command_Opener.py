f240a813e2b34821e50bae0c3acbcb8691be19b9
Roshan Malviya <roshanmalviya849@gmail.com>
2025-12-20 13:46:46 +0530

    Add files via upload

---
+++ Voice_Command_Opener.py
@@ -0,0 +1,11 @@
+import os
+
+cmd = input("Say command(open chrome / open notepad): ").lower()
+
+if "chrome" in cmd:
+    os.system("start chrome")
+elif "notepad" in cmd:
+    os.system("start notepad")
+else:
+    print("Command not recognized.")
+    
