e7acc08f77add6c44e36ab8d305eeaeed9a64981
Roshan Malviya <roshanmalviya849@gmail.com>
2026-02-25 19:58:56 +0530

    Add files via upload

---
+++ 3D-Building.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pyvista as pv
+
+plotter = pv.Plotter()
+for i in range(5):
+    for j in range(5):
+        h = np.random.randint(5,20)
+        box = pv.Box(bounds=(i,i+1,j,j+1,0,h))
+        plotter.add_mesh(box, color="lightgray")
+
+plotter.show(title="3D city Massing Model")
+print("hello")
